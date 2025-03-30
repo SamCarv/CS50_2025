3e37f539b9fd7d6942671c9aaef1525772040757
Samuel Carvalho <samcarvdev@gmail.com>
2025-03-30 18:33:58 -0300

    function and loops

--- week_01/cat.c
+++ week_01/cat.c
@@ -1,9 +1,23 @@
+/*#include <stdbool.h>*/
 #include <stdio.h>
 
-int main(void)
-{
-    printf("meow\n");
-    printf("meow\n");
-    printf("meow\n");
-    printf("meow\n");
+void meow(void);
+
+int main(void) {
+  /*int i = 0;*/
+  /**/
+  /*while (i < 3) {*/
+  /*  printf("meow\n");*/
+  /*  i++;*/
+  /*}*/
+
+  /*while (true) {*/
+  /*  printf("meow\n");*/
+  /*}*/
+
+  for (int i = 0; i < 3; i++) {
+    meow();
+  }
 }
+
+void meow(void) { printf("meow\n"); }
